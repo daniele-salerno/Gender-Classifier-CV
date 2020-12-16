778feb427915a1b3cddd3ffd4fa55e89dc02e41f
Jotunheim <salerno.daniele92@gmail.com>
2020-12-16 15:51:00 +0100

    new model

--- train_model_cnn.py
+++ train_model_cnn.py
@@ -8,7 +8,7 @@ BATCH_SIZE = 16
 
 # istanziamo il generatore...
 datagen = ImageDataGenerator(
-        validation_split = 0.3, # train sul 70%
+        validation_split = 0.1, # train sul 70%
         rescale = 1./255, # norm immagini
 
         # crezione immagini usa e getta aggiuntive
@@ -20,7 +20,7 @@ datagen = ImageDataGenerator(
 # ... per leggere le immagini direttamente da disco
 train_generator = datagen.flow_from_directory(
         DATASET_DIR,
-        target_size = (150,150), # ridimensiamo le immagini a dimensione comune
+        target_size = (200,200), # ridimensiamo le immagini a dimensione comune
         batch_size = BATCH_SIZE,
         class_mode = "binary", # uomo/donna
         subset = "training"
@@ -28,7 +28,7 @@ train_generator = datagen.flow_from_directory(
 
 test_generator = datagen.flow_from_directory(
         DATASET_DIR,
-        target_size = (150,150),
+        target_size = (200,200),
         batch_size = BATCH_SIZE,
         class_mode = "binary",
         subset = "validation" # test
@@ -39,7 +39,7 @@ print(train_generator.class_indices)
 
 model = Sequential()
  
-model.add(Conv2D(filters=64, kernel_size=4, padding="same", activation="relu", input_shape=(150,150, 3)))
+model.add(Conv2D(filters=64, kernel_size=4, padding="same", activation="relu", input_shape=(200,200, 3)))
 model.add(MaxPooling2D(pool_size=4, strides=4))
 model.add(Dropout(0.5))
 model.add(Conv2D(filters=32, kernel_size=4, padding="same", activation="relu"))
@@ -51,7 +51,7 @@ model.add(Dropout(0.5))
 model.add(Dense(1, activation="sigmoid"))
 
 model.compile(optimizer="adam", loss="binary_crossentropy", metrics=["accuracy"])
-model.fit(train_generator, epochs=40)
+model.fit(train_generator, epochs=60)
 
 metrics_train = model.evaluate(train_generator)
 metrics_test = model.evaluate(test_generator)
